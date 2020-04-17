90b8c8fc4df5a4c28911bbf6971e6e45026782bc
sdwru <40401953+sdwru@users.noreply.github.com>
2020-04-17 15:23:15 -0700

    Create Setting.php

---
+++ src/Entity/Setting.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the AwxV2 library.
+ *
+ * (c) Sdwru https://github.com/sdwru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AwxV2\Entity;
+
+/**
+ *
+ *
+ */
+final class Setting extends AbstractEntity
+{
+   
+}
