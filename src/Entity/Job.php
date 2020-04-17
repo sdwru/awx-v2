a364e38b24cbf3f927d25d427950cd68d898c65d
sdwru <40401953+sdwru@users.noreply.github.com>
2020-04-17 15:24:38 -0700

    Update Job.php

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the AwxV2 library.
  *
- * (c) Antoine Corcy <contact@sbin.dk>
+ * (c) Sdwru https://github.com/sdwru
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,8 +12,8 @@
 namespace AwxV2\Entity;
 
 /**
- * @author Yassir Hannoun <yassir.hannoun@gmail.com>
- * @author Graham Campbell <graham@alt-three.com>
+ *
+ *
  */
 final class Job extends AbstractEntity
 {
