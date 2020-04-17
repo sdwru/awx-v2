5bac55263f3e229e379222d0e04c9494bcf07f00
sdwru <40401953+sdwru@users.noreply.github.com>
2020-04-17 14:00:13 -0700

    Create Credential.php

---
+++ src/Entity/Credential.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the AwxV2 library.
+ *
+ * (c) Antoine Corcy <contact@sbin.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AwxV2\Entity;
+
+/**
+ * @author Yassir Hannoun <yassir.hannoun@gmail.com>
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+final class Credential extends AbstractEntity
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var object
+     */
+    public $related;
+    
+    /**
+     * @var string
+     */
+    public $name;
+}
