37388aa9bd89f8100b92c3e9634373f9d27a81d4
sdwru <40401953+sdwru@users.noreply.github.com>
2020-04-22 16:53:56 -0700

    Create Project.php

---
+++ src/Entity/Project.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the AwxV2 library.
+ *
+ * (c) Sdwru https://github.com/sdwru
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AwxV2\Entity;
+
+/**
+ *
+ *
+ */
+final class ProjectEntity extends AbstractEntity
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var object
+     */
+    public $related;
+    
+    /**
+     * @var string
+     */
+    public $name;
+    
+    /**
+     * @var string
+     */
+    public $playbook;
+    
+    /**
+     * @var string
+     */
+    public $job_type;
+}
